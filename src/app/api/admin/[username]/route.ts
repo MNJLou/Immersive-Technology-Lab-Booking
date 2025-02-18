195e34348f1fc6aff4844c54b350e5529b326387
MNJLou <jeromelou502@gmail.com>
2025-02-18 11:45:57 +0200

    Fixed api/admin/username issue with POST export

--- src/app/api/admin/[username]/route.ts
+++ src/app/api/admin/[username]/route.ts
@@ -2,9 +2,13 @@ import { connectMongo } from "@/utils/connectDB";
 import User from "@/models/userModel";
 import {NextRequest, NextResponse} from "next/server";
 
+interface Context {
+    params: { username: string };
+}
+
 // Used to fetch a user based on username (student number) and check if they are an admin
-export async function POST( request: NextRequest, {params}: { params: { username: string } } ) {
-    const { username } = params;
+export async function POST( request: NextRequest, context: Context ) {
+    const { username } = context.params;
     await connectMongo();
     // studentNumber: username
     const admin = await User.findOne( { studentNumber: username, isAdmin: true } );
