d35fd82ce7146e7f9f2220dae22da46a619c9057
MNJLou <jeromelou502@gmail.com>
2025-02-18 13:21:42 +0200

    Fixed api/admin/username Context

--- src/app/api/admin/[username]/route.ts
+++ src/app/api/admin/[username]/route.ts
@@ -2,13 +2,9 @@ import { connectMongo } from "@/utils/connectDB";
 import User from "@/models/userModel";
 import {NextRequest, NextResponse} from "next/server";
 
-interface Context {
-    params: { username: string };
-}
-
 // Used to fetch a user based on username (student number) and check if they are an admin
-export async function POST( request: NextRequest, context: Context ) {
-    const { username } = context.params;
+export async function POST( request: NextRequest, {params}: { params: { username: string } } ) {
+    const { username } = params;
     await connectMongo();
     // studentNumber: username
     const admin = await User.findOne( { studentNumber: username, isAdmin: true } );
